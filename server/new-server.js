dd9499933953e54d4265fe787e9db899159ab2e7
Ken Li <iliawnek@gmail.com>
2017-03-08 21:23:29 +0000

    Move GET account under /accounts instead of /accounts/<id>

--- server/new-server.js
+++ server/new-server.js
@@ -4,22 +4,25 @@ var port = process.env.PORT || 3001;
 var router = express.Router();
 var net = require('net');
 
+// Service hosts and ports - fixed for demonstration purposes only.
 var ACCOUNTS_SERVICE_PORT = 3031;
 var REPLAY_SERVICE_PORT = 9999;
 var REPLAY_SERVICE_HOST = '0.0.0.0';
 CREATE_TRANSACTION_SERVICE_PORT = 3032;
 
-router.route('/accounts/:number')
+// /accounts
+router.route('/accounts')
+
+  // GET
   .get(function (request, response) {
     var client = connect(ACCOUNTS_SERVICE_PORT);
     write(client, 'DATA GET', {
-      number: parseInt(request.params.number),
+      number: parseInt(request.query.number),
     });
     client.on('data', function (data) {
       var status = getStatus(data);
       var payload = getPayload(data);
       if (status !== 200) {
-        console.log(data);
         response.status(status).json({message: payload});
       } else {
         response.status(status).json(JSON.parse(payload));
@@ -27,9 +30,8 @@ router.route('/accounts/:number')
       client.destroy();
     });
   })
-;
 
-router.route('/accounts')
+  // POST
   .post(function (request, response) {
     var client = connect(ACCOUNTS_SERVICE_PORT);
     write(client, 'DATA POST', {
@@ -46,7 +48,10 @@ router.route('/accounts')
   })
 ;
 
+// /transactions
 router.route('/transactions')
+
+  // GET
   .get(function (request, response) {
     var client = connect(REPLAY_SERVICE_PORT, REPLAY_SERVICE_HOST);
     write(client, 'DATA GET', {
@@ -66,6 +71,7 @@ router.route('/transactions')
     })
   })
 
+  // POST
   .post(function (request, response) {
     var client = connect(CREATE_TRANSACTION_SERVICE_PORT);
     write(client, 'DATA POST', {
@@ -96,10 +102,12 @@ function write(client, command, params) {
   client.write(command + ' ' + JSON.stringify(params) + '\n');
 }
 
-// Parse incoming data from socket.
+// Extract HTTP status code from received data.
 function getStatus(data) {
   return parseInt(data.substring(0, 3));
 }
+
+// Extract payload from received data.
 function getPayload(data) {
   return data.substring(4);
 }
