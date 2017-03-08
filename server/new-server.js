bf852baaaa88f3cb52054b88a6b8655a90b4e0ca
Ken Li <iliawnek@gmail.com>
2017-03-08 21:17:22 +0000

    Return proper JSON object instead of string

--- server/new-server.js
+++ server/new-server.js
@@ -4,14 +4,26 @@ var port = process.env.PORT || 3001;
 var router = express.Router();
 var net = require('net');
 
+var ACCOUNTS_SERVICE_PORT = 3031;
+var REPLAY_SERVICE_PORT = 9999;
+var REPLAY_SERVICE_HOST = '0.0.0.0';
+CREATE_TRANSACTION_SERVICE_PORT = 3032;
+
 router.route('/accounts/:number')
   .get(function (request, response) {
-    var client = connect(3031);
+    var client = connect(ACCOUNTS_SERVICE_PORT);
     write(client, 'DATA GET', {
       number: parseInt(request.params.number),
     });
     client.on('data', function (data) {
-      response.json({data});
+      var status = getStatus(data);
+      var payload = getPayload(data);
+      if (status !== 200) {
+        console.log(data);
+        response.status(status).json({message: payload});
+      } else {
+        response.status(status).json(JSON.parse(payload));
+      }
       client.destroy();
     });
   })
@@ -19,14 +31,16 @@ router.route('/accounts/:number')
 
 router.route('/accounts')
   .post(function (request, response) {
-    var client = connect(3031);
+    var client = connect(ACCOUNTS_SERVICE_PORT);
     write(client, 'DATA POST', {
       number: parseInt(request.query.number),
-      holder: parseInt(request.query.holder),
+      holder: request.query.holder,
       start_balance: parseFloat(request.query.start_balance),
     });
     client.on('data', function (data) {
-      response.json({data});
+      var status = getStatus(data);
+      var payload = getPayload(data);
+      response.status(status).json({message: payload});
       client.destroy();
     });
   })
@@ -34,27 +48,35 @@ router.route('/accounts')
 
 router.route('/transactions')
   .get(function (request, response) {
-    var client = connect(9999, '0.0.0.0');
+    var client = connect(REPLAY_SERVICE_PORT, REPLAY_SERVICE_HOST);
     write(client, 'DATA GET', {
       account: parseInt(request.query.account),
       start_date: parseInt(request.query.start_date),
       end_date: parseInt(request.query.end_date),
     });
     client.on('data', function (data) {
-      response.json({data});
+      var status = getStatus(data);
+      var payload = getPayload(data);
+      if (status !== 200) {
+        response.status(status).json({message: payload});
+      } else {
+        response.status(status).json(JSON.parse(payload));
+      }
       client.destroy();
     })
   })
 
   .post(function (request, response) {
-    var client = connect(3032);
+    var client = connect(CREATE_TRANSACTION_SERVICE_PORT);
     write(client, 'DATA POST', {
       from: parseInt(request.query.from),
       to: parseInt(request.query.to),
       amount: parseFloat(request.query.amount),
     });
     client.on('data', function (data) {
-      response.json({data});
+      var status = getStatus(data);
+      var payload = getPayload(data);
+      response.status(status).json({message: payload});
       client.destroy();
     });
   })
@@ -72,6 +94,14 @@ function connect(port, host) {
 function write(client, command, params) {
   console.log(JSON.stringify(params));
   client.write(command + ' ' + JSON.stringify(params) + '\n');
+}
+
+// Parse incoming data from socket.
+function getStatus(data) {
+  return parseInt(data.substring(0, 3));
+}
+function getPayload(data) {
+  return data.substring(4);
 }
 
 app.use('/api', router);
